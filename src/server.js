90cf109cf0ba48745bcddc739a8cc9bf726a8650
Oleksandr Krokhmalnyi <magr0s.ak@gmail.com>
2021-02-13 00:10:14 +0200

    feat: Use request body parser

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const apiRoutes = require('./api');
+
 
 const app = express();
 
-app.use('/', apiRoutes)
+app.use(bodyParser.json())
+  .use('/', apiRoutes)
   .get('*', (_, res) => (
     res.status(404)
       .send('Endpoint not found.')
